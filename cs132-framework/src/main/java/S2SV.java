2ca30fe9ca3663dd29e229c7523c5336d8a98db5
Michelle Sun <lu.yingshan@gmail.com>
2025-05-29 01:24:44 -0700

    (7/100) Translation: (print, add, subtract, mult, comp, and, while, not)
    (8) Total Passed:
        1-PrintLiteral
        2-Add
        6-Subtract
        7-Multiply
        8-Compare
        9-And
        NotExpression
        WhileStatement

--- cs132-framework/src/main/java/S2SV.java
+++ cs132-framework/src/main/java/S2SV.java
@@ -1,12 +1,14 @@
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import IR.SparrowParser;
 import IR.visitor.SparrowConstructor;
 import IR.syntaxtree.Node;
+import IR.token.*;
+
+import sparrowv.Instruction;
+import sparrowv.*;
 
 public class S2SV {
     public static void main(String [] args) throws Exception {
@@ -19,39 +21,70 @@ public class S2SV {
         sparrow.Program program = constructor.getProgram();
         System.err.println(program.toString());
 
-        //// fast liveness analysis
-        HashMap<String, LiveInterval> intervals_map = new HashMap<>();
-        FastLivenessVisitor lv = new FastLivenessVisitor();
-        lv.visit(program);
-        intervals_map = lv.intervals_map;
+        // //// FAST LIVENESS ANALYSIS
+        // HashMap<String, LiveInterval> intervals_map = new HashMap<>();
+        // FastLivenessVisitor lv = new FastLivenessVisitor();
+        // lv.visit(program);
+        // intervals_map = lv.intervals_map;
 
-        // print results
-        for (Map.Entry<String, LiveInterval> entry : intervals_map.entrySet()) {
-            System.err.println(entry.getKey() + " = " + entry.getValue());
-        }
+        // // print results
+        // for (Map.Entry<String, LiveInterval> entry : intervals_map.entrySet()) {
+        //     System.err.println(entry.getKey() + " = " + entry.getValue());
+        // }
 
-        System.err.println("📍 Label to Line Map:");
-        for (Map.Entry<String, Integer> entry : lv.labelToLine.entrySet()) {
-            System.err.println("  " + entry.getKey() + " : line " + entry.getValue());
-        }
+        // System.err.println("📍 Label to Line Map:");
+        // for (Map.Entry<String, Integer> entry : lv.labelToLine.entrySet()) {
+        //     System.err.println("  " + entry.getKey() + " : line " + entry.getValue());
+        // }
 
-        System.err.println("🔁 Detected Loop Ranges:");
-        for (LoopRange loop : lv.loopRanges) {
-            System.err.println("  Loop from line " + loop.start + " to line " + loop.end);
-        }
+        // System.err.println("🔁 Detected Loop Ranges:");
+        // for (LoopRange loop : lv.loopRanges) {
+        //     System.err.println("  Loop from line " + loop.start + " to line " + loop.end);
+        // }
 
-        //// Linear Scan Register Allocation
-        // convert to List<LivenessInterval>
-        List<LiveInterval> intervalList = new ArrayList<>();
-        for (var entry : lv.intervals_map.entrySet()) {
-            intervalList.add(new LiveInterval(entry.getKey(), entry.getValue().start, entry.getValue().end));
-        }
+        // //// LINEAR SCAN REGISTER ALLOCATION
+        // // convert to List<LivenessInterval>
+        // List<LiveInterval> intervalList = new ArrayList<>();
+        // for (var entry : lv.intervals_map.entrySet()) {
+        //     intervalList.add(new LiveInterval(entry.getKey(), entry.getValue().start, entry.getValue().end));
+        // }
 
-        // choose 3 registers: t0, t1, t2
-        List<String> regs = new ArrayList<>(List.of("t0", "t1", "t2"));
-        LinearScanRegisterAllocator allocator = new LinearScanRegisterAllocator(intervalList, 3, regs);
-        allocator.allocate();
-        allocator.printResult();
+        // // choose 3 registers: t0, t1, t2
+        // List<String> regs = new ArrayList<>(List.of("t0", "t1", "t2"));
+        // LinearScanRegisterAllocator allocator = new LinearScanRegisterAllocator(intervalList, 3, regs);
+        // allocator.allocate();
+        // allocator.printResult();
 
+        //// TRANSLATION
+        // TranslationVisitor tv = new TranslationVisitor();
+        // // lv.visit(program);
+        // List<Instruction> translated_instr = tv.visit(program);
+        // System.err.println("🌷 🌷 🌷 🌷 🌷 FINAL PROGRAM!! (.err.) 🌷 🌷 🌷 🌷 🌷");
+        // for (Instruction instr : translated_instr) {
+        //     System.err.println(instr.toString());
+        // }
+        // System.err.println("🌷 🌷 🌷 🌷 🌷 FINAL PROGRAM!! (.out.) 🌷 🌷 🌷 🌷 🌷");
+        // for (Instruction instr : translated_instr) {
+        //     System.out.println(instr.toString());
+        // }
+
+        // in a variables liveness variable, if u can track a func call, then alloc to a callee save register so you don't have to do reallocation
+        // its just a flag, and just go to first pass and hit a call and iterate through all the ur variable intervals and check them off
+        // if you can multiple calls to a funct, if you save to a callee save register, then you have less calls to the stack
+
+        // List<FunctionDecl> vFuncs = new ArrayList<>();
+        // TranslationVisitor tv = new TranslationVisitor();
+
+        // for (sparrow.FunctionDecl f : program.funDecls) {
+        //     List<Instruction> instrs = f.accept(tv);
+        //     FunctionDecl vFunc = new FunctionDecl(f.functionName, f.formalParameters, f.block);
+        //     vFuncs.add(vFunc);
+        // }
+
+        // sparrowv.Program vProg = new sparrowv.Program(vFuncs);
+        // System.out.println(vProg);
+
+        TranslationVisitor tv = new TranslationVisitor();
+        tv.visit(program);
     }
 }
---
+++ cs132-framework/src/main/java/TranslationVisitor.java
@@ -0,0 +1,246 @@
+// must be sparrowv.Instruction !!
+import sparrowv.*;
+import IR.token.*;
+
+import sparrow.visitor.RetVisitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TranslationVisitor implements RetVisitor < List<sparrowv.Instruction> >{
+
+    private final Register t0 = new Register("t0");
+    private final Register t1 = new Register("t1");
+
+    List<sparrowv.FunctionDecl> func_list;
+    List<sparrowv.Instruction> instr_list_global;
+    Identifier ret_id = null;
+
+    // id to reg
+    private Register id_to_reg(Identifier id) {
+        return new Register(id.toString());
+    }
+
+    // wrap an instr w a list
+    private List<Instruction> wrap(Instruction i) {
+        List<Instruction> l = new ArrayList<>();
+        l.add(i);
+        return l;
+    }
+
+    // VISIT METHODS START HERE
+    
+    /*   List<FunctionDecl> funDecls; */
+    @Override
+    public List<Instruction> visit(sparrow.Program n){
+        func_list = new ArrayList<>();
+
+        for (sparrow.FunctionDecl fd: n.funDecls){
+            fd.accept(this);
+        }
+
+        sparrowv.Program program = new sparrowv.Program(func_list);
+        System.err.println("🌷 🌷 🌷 🌷 🌷 FINAL PROGRAM!! (.err.) 🌷 🌷 🌷 🌷 🌷");
+        System.err.println(program);
+        System.err.println("🌷 🌷 🌷 🌷 🌷 FINAL PROGRAM!! (.out.) 🌷 🌷 🌷 🌷 🌷");
+        System.out.println(program);
+        return null;
+        // List<Instruction> result = new ArrayList<>();
+        // for (sparrow.FunctionDecl f : n.funDecls) {
+        //     result.addAll(f.accept(this));
+        // }
+        // return result;
+    }
+
+    /*   Program parent;
+    *   FunctionName functionName;
+    *   List<Identifier> formalParameters;
+    *   Block block; */
+    @Override
+    public List<Instruction> visit(sparrow.FunctionDecl n){
+        sparrowv.Block b = new sparrowv.Block(n.block.accept(this), ret_id);
+        sparrowv.FunctionDecl func = new sparrowv.FunctionDecl(n.functionName, n.formalParameters, b);
+        func_list.add(func);
+        return null;
+        // return n.block.accept(this); // eventually wrap in a FunctionDecl in full translator
+    }
+
+    /*   FunctionDecl parent;
+    *   List<Instruction> instructions;
+    *   Identifier return_id; */
+    @Override
+    public List<Instruction> visit(sparrow.Block n){
+        List<Instruction> instr_list = new ArrayList<>();
+        for (sparrow.Instruction s : n.instructions) {
+            instr_list.addAll(s.accept(this));
+        }
+        ret_id = n.return_id;
+        instr_list.add(new Move_Reg_Id(t0, n.return_id));
+        return instr_list;
+    }
+
+    /*   Label label; */
+    @Override
+    public List<Instruction> visit(sparrow.LabelInstr n){
+        return wrap(new sparrowv.LabelInstr(n.label));
+    }
+
+    /*   Identifier lhs;
+    *   int rhs; */
+    @Override
+    public List<Instruction> visit(sparrow.Move_Id_Integer n){
+        List<Instruction> instr_list = new ArrayList<>();
+        instr_list.add(new Move_Reg_Integer(t0, n.rhs));
+        instr_list.add(new Move_Id_Reg(n.lhs, t0));
+        return instr_list;
+        // return wrap(new Move_Reg_Integer(id_to_reg(n.lhs), n.rhs));
+    }
+
+    /*   Identifier lhs;
+    *   FunctionName rhs; */
+    @Override
+    public List<Instruction> visit(sparrow.Move_Id_FuncName n){
+        return wrap(new Move_Reg_FuncName(id_to_reg(n.lhs), n.rhs));
+    }
+
+    /*   Identifier lhs;
+    *   Identifier arg1;
+    *   Identifier arg2; */
+    @Override
+    public List<Instruction> visit(sparrow.Add n){
+        List<Instruction> instrs = new ArrayList<>();
+        instrs.add(new Move_Reg_Id(t0, n.arg1));
+        instrs.add(new Move_Reg_Id(t1, n.arg2));
+        instrs.add(new sparrowv.Add(t0, t0, t1));
+        instrs.add(new Move_Id_Reg(n.lhs, t0));
+        return instrs;
+    }
+
+    /*   Identifier lhs;
+    *   Identifier arg1;
+    *   Identifier arg2; */
+    @Override
+    public List<Instruction> visit(sparrow.Subtract n){
+        List<Instruction> instrs = new ArrayList<>();
+        instrs.add(new Move_Reg_Id(t0, n.arg1));
+        instrs.add(new Move_Reg_Id(t1, n.arg2));
+        instrs.add(new sparrowv.Subtract(t0, t0, t1));
+        instrs.add(new Move_Id_Reg(n.lhs, t0));
+        return instrs;
+    }
+
+    /*   Identifier lhs;
+    *   Identifier arg1;
+    *   Identifier arg2; */
+    @Override
+    public List<Instruction> visit(sparrow.Multiply n){
+        List<Instruction> instrs = new ArrayList<>();
+        instrs.add(new Move_Reg_Id(t0, n.arg1));
+        instrs.add(new Move_Reg_Id(t1, n.arg2));
+        instrs.add(new sparrowv.Multiply(t0, t0, t1));
+        instrs.add(new Move_Id_Reg(n.lhs, t0));
+        return instrs;
+    }
+
+    /*   Identifier lhs;
+    *   Identifier arg1;
+    *   Identifier arg2; */
+    @Override
+    public List<Instruction> visit(sparrow.LessThan n){
+        List<Instruction> instrs = new ArrayList<>();
+        instrs.add(new Move_Reg_Id(t0, n.arg1));
+        instrs.add(new Move_Reg_Id(t1, n.arg2));
+        instrs.add(new sparrowv.LessThan(t0, t0, t1));
+        instrs.add(new Move_Id_Reg(n.lhs, t0));
+        return instrs;
+    }
+
+    /*   Identifier lhs;
+    *   Identifier base;
+    *   int offset; */
+    @Override
+    public List<Instruction> visit(sparrow.Load n){
+        return List.of(
+            new Move_Reg_Id(t0, n.base),
+            new sparrowv.Load(t0, t0, n.offset),
+            new Move_Id_Reg(n.lhs, t0)
+        );
+    }
+
+    /*   Identifier base;
+    *   int offset;
+    *   Identifier rhs; */
+    @Override
+    public List<Instruction> visit(sparrow.Store n){
+        return List.of(
+            new Move_Reg_Id(t0, n.base),
+            new Move_Reg_Id(t1, n.rhs),
+            new sparrowv.Store(t0, n.offset, t1)
+        );
+    }
+
+    /*   Identifier lhs;
+    *   Identifier rhs; */
+    @Override
+    public List<Instruction> visit(sparrow.Move_Id_Id n){
+        return List.of(
+            new Move_Reg_Id(t0, n.rhs),
+            new Move_Id_Reg(n.lhs, t0)
+        );
+    }
+
+    /*   Identifier lhs;
+    *   Identifier size; */
+    @Override
+    public List<Instruction> visit(sparrow.Alloc n){
+        return List.of(
+            new Move_Reg_Id(t0, n.size),
+            new sparrowv.Alloc(t0, t0),
+            new Move_Id_Reg(n.lhs, t0)
+        );
+    }
+
+    /*   Identifier content; */
+    @Override
+    public List<Instruction> visit(sparrow.Print n){
+        return List.of(
+            new Move_Reg_Id(t0, n.content),
+            new sparrowv.Print(t0)
+        );
+    }
+
+    /*   String msg; */
+    @Override
+    public List<Instruction> visit(sparrow.ErrorMessage n){
+        return wrap(new sparrowv.ErrorMessage(n.msg));
+    }
+
+    /*   Label label; */
+    @Override
+    public List<Instruction> visit(sparrow.Goto n){
+        return wrap(new sparrowv.Goto(n.label));
+    }
+
+    /*   Identifier condition;
+    *   Label label; */
+    @Override
+    public List<Instruction> visit(sparrow.IfGoto n){
+        return List.of(
+            new Move_Reg_Id(t0, n.condition),
+            new sparrowv.IfGoto(t0, n.label)
+        );
+    }
+
+    /*   Identifier lhs;
+    *   Identifier callee;
+    *   List<Identifier> args; */
+    @Override
+    public List<Instruction> visit(sparrow.Call n){
+        List<Instruction> instrs = new ArrayList<>();
+        // instrs.add(new Move_Reg_Id(t0, n.lhs));
+        instrs.add(new Move_Reg_Id(t0, n.callee));
+        instrs.add(new sparrowv.Call(t0, t0, n.args));
+        instrs.add(new Move_Id_Reg(n.lhs, t0));
+        return instrs;
+    }
+}
